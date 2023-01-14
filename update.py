58a8ce35239c11783f0f972e1f87bdf68e8ef4f7
Denys Plakhtii <dplakhtiy1103@gmail.com>
2023-01-14 08:30:34 +0200

    Fixed update.py files rewriting

--- update.py
+++ update.py
@@ -6,14 +6,16 @@ aPath = os.path.dirname(os.path.abspath(__file__))
 aFiles = os.listdir(aPath + '/')
 aFiles.sort()
 
+os.system(f"rm -rf {aPath}/pages/*.html") # Deleting all pages for replace it
+
 corrects_shortnames = ["JS", "LN", "PS", "PY"] # list of "startswith" good files
 
 for file in aFiles:
     if file.endswith(".md"):
         starts_with = file[:2] # 2 first letters of file
         if starts_with in corrects_shortnames:
-            with open(f"{aPath}/pages/{starts_with}.html", "w") as f:
+            with open(f"{aPath}/pages/{starts_with}.html", "a") as f:
                 f.write(f'<zero-md src="/{file}"></zero-md><hr />' + os.linesep) 
         else:
-            with open(f"{aPath}/pages/XZ.html", "w") as f:
+            with open(f"{aPath}/pages/XZ.html", "a") as f:
                 f.write(f'<zero-md src="/{file}"></zero-md><hr />' + os.linesep) 
