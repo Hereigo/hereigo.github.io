3fc854497249049ec1c9ac2bf35758d7163bcd76
ELEKS-SOFTWARE\andrii.plakhtii <andrii.plakhtii@eleks.com>
2023-04-11 20:53:11 +0300

    py update

--- py/1_main.py
+++ py/1_main.py
@@ -1,104 +1,28 @@
-class Parent:
-    def __init__(self, age, name, gender):
-        self._name = name
-        self.gender = gender
-        self.__age = age  # - is private but accesible as "parent_1._Parent__age"
-
-    def set_age(self, value):  # - private fields should be set by "setters"
-        if not isinstance(value, (int, float)):
-            raise ValueError('Only numbers allowed!')
-        self.__age = value
-
-    def get_age(self):
-        return self.__age
-
-    age = property(fget=get_age, fset=set_age)
-
-    def get_name_and_age(self):
-        return self._name, self.__age
-
- # Private is Accessible!
-parent_1 = Parent(age='too Old!', name='Jolie', gender='female')
-parent_1.__age = '100'  # - Allowed!
-parent_1._age = '100'  # - Allowed!
-# parent_1.age = '100' - NOT Allowed!
-
-print('\r\n Parent-1', parent_1.get_name_and_age(), 'gender =', parent_1.gender)
-
-
-class Parent2:
-    def __init__(self):
-        pass
-
-    def parent2_print_hi(self):
-        print('Hi!')
-
-
-class Child(Parent, Parent2):  # ORDER matters!
-    def __init__(self, age, name):
-        super().__init__(age, name, 'female')  # First Parent init.
-        # Parent.__init__(self, age, name, 'female')
-        Parent2.__init__(self)
-
-
-child = Child(13, 'Jess')
-
-print('\r\nChild', child.get_name_and_age(), '\r\n')
-
-child.parent2_print_hi()
-
-print(Child.__mro__, '\r\n')  # Method Resolution Order.
-
-# ( <class '__main__.Child'>, <class '__main__.Parent'>, <class '__main__.Parent2'>, <class 'object'> )
-
-print(dir(Child))  # To see all PRIVATE and INHERITED members of CHILD.
-
-# ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__',
-# '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__',
-# '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__','__sizeof__', '__str__',
-# '__subclasshook__', '__weakref__', 'age', 'get_age', 'get_name_and_age', 'parent2_print_hi', 'set_age']
-
-print()
-
-# # # # # # Comlex Objects :
-
-user = {
-    "name": "John Doe",
-    "info": {
-        "basic": {
-            "age": 25,
-            "salary": 5000
-        },
-        "additional": {
-            "study": "mathematics",
-            "family": "married"
-        },
-        "special": {
-            "projects": [
-                {"name": "quantum computing", "stage": "in progress"},
-                {"name": "laser gun", "stage": "in production"}
-            ]
-        }
+def func1(idx, count):
+    return {
+        "ID": idx,
+        "values": ["{}-{}".format(idx, val) for val in range(count)]
     }
-}
 
 
-def get_data_from_dict(dict, keys):
-    data = dict
-    for key in keys:
-        data = data[key]
-    return data
+a = list(range(5))[::-1]  # -   [4, 3, 2, 1, 0]
+
+z = list(zip('abcdefg', range(3), range(999)))
+#   -   [('a', 0, 0), ('b', 1, 1), ('c', 2, 2)])
 
 
-print(get_data_from_dict(user, ['info', 'special', 'projects']))
-print()
+def generate(count):
+    return [func1(i, c) for i, c in zip(range(count), list(range(count))[::-1])]
 
 
-def get_data_reccurin(dict, path_to_prop, idx=0):
-    if idx < len(path_to_prop):
-        return get_data_reccurin(dict[path_to_prop[idx]], path_to_prop, idx+1)
-    return dict
+x = generate(5)
+# [ {'ID': 0, 'values': ['0-0', '0-1', '0-2', '0-3']},
+# {'ID': 1, 'values': ['1-0', '1-1', '1-2']},
+# {'ID': 2, 'values': ['2-0', '2-1']},
+# {'ID': 3, 'values': ['3-0']},
+# {'ID': 4, 'values': []} ]
 
+y = [val for sublist in x for val in sublist['values']]
+# - ['0-0', '0-1', '0-2', '0-3', '1-0', '1-1', '1-2', '2-0', '2-1', '3-0']
 
-print(get_data_reccurin(user, ['info', 'special', 'projects']))
-print()
+print(y)
