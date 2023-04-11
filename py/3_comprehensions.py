d13d2eae4f9a40122a115f2ac9c1621535409d79
ELEKS-SOFTWARE\andrii.plakhtii <andrii.plakhtii@eleks.com>
2023-04-11 12:13:41 +0300

    py upd

---
+++ py/3_comprehensions.py
@@ -0,0 +1,8 @@
+# List Comprehensions:
+chars = [char for char in "qwerty"]  # ['q', 'w', 'e', 'r', 't', 'y']
+print(chars)
+
+names = [n.capitalize() for n in ['mike', 'sally', 'jack']]  # ['Mike', ...
+print(names)
+
+even_nums
