62f3393c531110e8928e5dfdbfad4cb428c214b1
ELEKS-SOFTWARE\andrii.plakhtii <andrii.plakhtii@eleks.com>
2023-05-24 21:11:30 +0300

    calendar global update

--- xtra/search.js
+++ xtra/search.js
@@ -1,4 +1,4 @@
-const mySearch = document.querySelector('button[type="submit"]');
+const mySearch = document.querySelector('input[type="submit"]');
 const mySearchField = document.querySelector('input[type="search"]');
 mySearch.addEventListener('click', function () {
   const letters = ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ї', 'ф', 'і', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'є', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю'];
