f84e0b1767ac25411106fbf58c2f05d6cead9780
Hereigo <hereigo@online.ua>
2023-08-23 19:15:02 +0300

    cal upd

--- data.js
+++ data.js
@@ -47,11 +47,10 @@ const aData = [
 	{ m: 00, d: 23, n: "300 Troylana" },
 	{ m: 00, d: 28, n: "160 iLanos" },
 	// ----- 4weekly -----
-	{ m: 08, d: 25, n: "HR meet (28)" },
 	{ m: 08, d: 31, n: "160 Life-M (28)" },
 	{ m: 08, d: 31, n: "4G Chck GB (28)" },
 	{ m: 09, d: 03, n: "225 Life-4G (28)" },
 	{ m: 09, d: 13, n: "155 U-KS (28)" },
 	// ----- only once -----
-	{ m: 08, d: 23, n: "16:00 Dantis BD" },
+	{ m: 08, d: 25, n: "11:00 - HR meet" },
 ];
