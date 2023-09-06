cf3a5d70e86e1bc3d8952619cf017fdab030c1ff
Hereigo <hereigo@online.ua>
2023-09-06 19:45:18 +0300

    update mongo till 6.0

--- node-js/app/routes/note_routes.js
+++ node-js/app/routes/note_routes.js
@@ -1,14 +1,27 @@
 module.exports = function (app, db) {
 
-    app.get('/', (req, res) => {
+    app.get('/notes', (req, res) => {
         res.send('Site is under construction.')
     });
 
     // Receiving -
     // Content-type : application/x-www-form-urlencoded
-    app.post('/notes', (req, res) => {
-        console.log(req.body)
-        res.send('Hello')
-    });
+    // app.post('/notes', (req, res) => {
+    //     console.log(req.body)
+    //     res.send('Hello')
+    // });
+
+    const collection =
+        app.post('/notes', (req, res) => {
+            const note = { text: req.body.body, title: req.body.title };
+            db.collection('notes').insert(note, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.send({ 'error': 'An error has occurred' });
+                } else {
+                    res.send(result.ops[0]);
+                }
+            });
+        });
 
 };
--- node-js/server.js
+++ node-js/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-const db = require('./config/db');
+const dbcfg = require('./config/db');
 
 const app = express();
 const port = 8000;
@@ -15,17 +15,39 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //     console.log('We are live on ' + port);
 // });
 
-MongoClient.connect(db.url, (err, database) => {
+// MongoClient.connect(dbcfg.url, (err, client) => {
+//     if (err) { return console.log('1st Err: ', err); }
+//     // Make sure you add the database name and not the collection name
+//     const db = client.db("test")
+//     // (for MongoDB 3.0+ only)
+//     require('./app/routes')(app, db);
+//     app.listen(port, () => {
+//         console.log('We are live on ' + port);
+//     });
+// });
 
-    if (err) { return console.log(err); }
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-    // Make sure you add the database name and not the collection name
-    // (for MongoDB 3.0+ only)
-    // const database = database.db("note-api");
-    require('./app/routes')(app, database);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(dbcfg.url, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
-    app.listen(port, () => {
-        console.log('We are live on ' + port);
-    });
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
 
-})
+run().catch(console.dir);
