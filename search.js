50299af3ddf8e58f562d34a833c81e4613c1bfc2
Hereigo <hereigo@online.ua>
2023-09-26 20:18:18 +0300

    cal upd

--- search.js
+++ search.js
@@ -3,7 +3,7 @@ const mySearchField = document.querySelector('input[type="search"]');
 
 mySearch.addEventListener('click', function () {
   let serachWords = mySearchField.value.toLowerCase().trim().replaceAll(' ', '+');
-  let serachReq = 'https://www.google.com.ua/search?q=' + serachWords + ' ua -.ru';
+  let serachReq = 'https://www.google.com.ua/search?q=' + serachWords + ' ua -.ru -/ru. -/ru/';
   window.open(serachReq);
 });
 
