39e89f09bcbfa2f3850523e6875829f41b21bd9f
Hereigo <hereigo@online.ua>
2023-12-05 18:35:49 +0200

    add sqlite

--- node_gen/server_express.js
+++ node_gen/server_express.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const port = 3000;
 
 const sqlite3 = require('sqlite3').verbose();
-const dbName = 'sqlite_tasks.db';
+const db = new sqlite3.Database('sqlite_tasks.db');
 
 let tasks = [
     { id: 1, text: 'To do a 0.1' },
@@ -13,27 +14,41 @@ let tasks = [
     { id: 5, text: 'To do a 0.5' }
 ];
 
+const serverErrorHandle = (err, res) => {
+    if (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.status(200).json(tasks);
+    db.all('SELECT * FROM tasks', (err, rows) => {
+        serverErrorHandle(err, res);
+        res.status(200).json(rows);
+    });
 });
 
 app.post('/tasks', (req, res) => {
     const newTask = req.body;
-    tasks.push(newTask);
-    res.status(201).json(newTask);
+    db.run('INSERT INTO tasks (text) VALIES (?)', [newTask.text], (err) => {
+        serverErrorHandle(err, res);
+    });
+    res.status(201).json({ id: this.lastID });
 });
 
 app.put('/tasks/:id', (req, res) => {
-    const taskToUpdate = req.body;
+    const { textAbc } = req.body;
     const taskId = parseInt(req.params.id);
-    const foundTask = tasks.find(t => t.id === taskId);
-    if (!foundTask) {
-        res.status(404).json({ message: 'Task not found.' });
-    }
-    foundTask.text = taskToUpdate;
-    res.status(200).json(foundTask);
+    // const foundTask = tasks.find(t => t.id === taskId);
+    // if (!foundTask) {
+    //     res.status(404).json({ message: 'Task not found.' });
+    // }
+    // foundTask.text = taskToUpdate;
+    db.run('INSERT INTO tasks SET text = ? WHERE id = ?', [taskToUpdate.text, taskId], (err) => {
+        serverErrorHandle(err, res);
+        res.status(201).json({ id: taskId, textAbc });
+    });
 });
 
 app.delete('/tasks/:id', (req, res) => {
@@ -42,6 +57,8 @@ app.delete('/tasks/:id', (req, res) => {
     res.status(204).json(tasks);
 });
 
-app.listen(() => {
-    console.log(`Server listening on http://localhost:3000`);
+
+
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}`);
 });
