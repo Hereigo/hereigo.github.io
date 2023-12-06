c9b932d215ec208821f4d1fa95f61dd00ddfb11b
Hereigo <hereigo@online.ua>
2023-12-06 13:29:46 +0200

    update sqlite

--- node_gen/sqlite_init.sql
+++ node_gen/sqlite_init.sql
@@ -2,7 +2,14 @@
 -- and Create Database in "Command Pallete"
 -- 
 -- SQLite
-CREATE TABLE tasks (
-    id INTEGER PRIMARY KEY,
-    text TEXT
-)
+-- To run Query => Shift + Ctrl + Q
+--
+-- CREATE TABLE tasks (
+--     id INTEGER PRIMARY KEY,
+--     text TEXT
+-- )
+-- 
+-- INSERT INTO tasks
+-- ('text') VALUES ('Asdf');
+
+SELECT * FROM tasks;
