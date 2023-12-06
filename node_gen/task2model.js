4e9f9b7b24e5a34662956ba5664da8792587c788
Hereigo <hereigo@online.ua>
2023-12-06 21:35:36 +0200

    soma updates

---
+++ node_gen/task2model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    text: {
+        type: 'string',
+        require: [true, 'Task description is required!']
+    },
+    isCompleted: {
+        type: 'boolean',
+        default: false
+    }
+});
+
+const Task = mongoose.model
