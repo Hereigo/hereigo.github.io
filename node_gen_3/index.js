9b05cdcc546aee7f5acaafde1fe222a1a50e2800
Hereigo <hereigo@online.ua>
2023-12-13 12:20:31 +0200

    read file

--- node_gen_3/index.js
+++ node_gen_3/index.js
@@ -1,7 +1,21 @@
 ﻿const http = require('http');
 const fs = require('fs');
+const { readFile } = require('fs/promises');
+
+async function readFileAsync(filePath) {
+    try {
+        const fileData = await readFile(filePath);
+        console.log(fileData.toString());
+    } catch (error) {
+        console.error('File reading error: ', error);
+    }
+}
+
+// TEST
+readFileAsync('hello.txt');
 
 http.createServer(function (req, res) {
+    // fs.readFileSync - Not Recommended for Express!!!
     fs.readFile('hello.txt', (err, fileData) => {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(fileData);
@@ -9,4 +23,4 @@ http.createServer(function (req, res) {
     })
 }).listen(3000);
 
-console.log('Serv er listening on http://localhost:3000');
+console.log('Server is listening on http://localhost:3000');
