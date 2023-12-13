ffc7e47d6b42a92b434c786e2fa47d3b57789507
Hereigo <hereigo@online.ua>
2023-12-13 13:25:51 +0200

    fs.open

--- node_gen_3/index.js
+++ node_gen_3/index.js
@@ -1,10 +1,11 @@
 ﻿const http = require('http');
 const fs = require('fs');
 const { readFile, appendFile, writeFile } = require('fs/promises');
-// writeFile - can creates file if not exists.
+// writeFile - can creates a file if not exists.
+
+const testFile = 'hello.txt';
 
 async function appendFileAsync(filePath, writeData) {
-
     try {
         await appendFile(filePath, writeData, { flag: 'a' }); // 'w'
         console.log('Appended successfully.');
@@ -22,18 +23,34 @@ async function readFileAsync(filePath) {
     }
 }
 
+// FS.OPEN with WRITE and CLOSE chain:
+// --------------------------------------------------
+fs.open(testFile, 'a', (err, file) => {
+    // ---- Callback function After Open:
+    if (err) throw err;
+    fs.write(file, 'FS.OPEN new text...', (err) => {
+        // ---- Callback function After Write:
+        if (err) throw err;
+        console.log('Text successfully added.');
+        fs.close(file, (err) => {
+            // ---- Callback function After Close:
+            if (err) throw err;
+            console.log('Successfully finished.');
+        });
+    });
+});
+
 // TEST
-readFileAsync('hello.txt');
-appendFileAsync('hello.txt', 'New appended text...\r\n');
-readFileAsync('hello.txt');
+appendFileAsync(testFile, 'New appended text...\r\n');
+readFileAsync(testFile);
 
 http.createServer(function (req, res) {
     // fs.readFileSync - Not Recommended for Express!!!
-    fs.readFile('hello.txt', (err, fileData) => {
+    fs.readFile(testFile, (err, fileData) => {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(fileData);
         return res.end();
     })
 }).listen(3000);
 
-console.log('Server is listening on http://localhost:3000');
+console.log('\r\nServer is listening on http://localhost:3000');
