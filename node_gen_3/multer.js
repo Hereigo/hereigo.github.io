7b98ff480a8fe946fcbfd821a510c0d85a7d2b7e
Hereigo <hereigo@online.ua>
2023-12-13 15:42:47 +0200

    file upload

--- node_gen_3/multer.js
+++ node_gen_3/multer.js
@@ -1,9 +1,8 @@
 ﻿const express = require('express');
 const multer = require('multer');
-
 const xpsApp = express();
-
-const upload = multer({ dest: 'uploads/' }).single('demo_image');
+//                                              request form-data KEY
+const upload = multer({ dest: 'uploads/' }).single("demo_image");
 
 xpsApp.get('/', (req, res) => {
     return res.send('Server is running.');
@@ -13,10 +12,22 @@ xpsApp.post('/img', (req, res) => {
     upload(req, res, (err) => {
         if (err)
             return res.status(400).send('Bad request.');
-        return res.send(req.file);
+
+        res.send(req.file);
+        // Response File Metadata:
+        // {
+        //     "fieldname": "demo_image",
+        //     "originalname": "Bla-bla-bla.png",
+        //     "encoding": "7bit",
+        //     "mimetype": "image/png",
+        //     "destination": "uploads/",
+        //     "filename": "606a35b560fc125207313e71c5f794e2",
+        //     "path": "uploads\\606a35b560fc125207313e71c5f794e2",
+        //     "size": 641909
+        // }
     });
 });
 
 xpsApp.listen(3000, () => {
-    console.log('\r\nServer is listening on http://localhost:3000');
+    console.log('Server is listening on http://localhost:3000');
 });
