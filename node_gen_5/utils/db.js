e7cfb5b27890487349f5cbf8dd5de79431b0f28b
Hereigo <hereigo@online.ua>
2023-12-30 20:27:16 +0200

    final app start

---
+++ node_gen_5/utils/db.js
@@ -0,0 +1,9 @@
+﻿import { connect } from 'mongoose';
+
+import { URI } from '../config/db.js';
+
+connect(URI).then(() => {
+    console.log('Successfully connected to DB.');
+}).catch((err) => {
+    console.err(err);
+});
