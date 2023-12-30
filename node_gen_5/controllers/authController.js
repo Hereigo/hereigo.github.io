05b9bfbb9170a51356e221ecb3ac1e1b2c0e7eb7
Hereigo <hereigo@online.ua>
2023-12-30 21:08:31 +0200

    half save

---
+++ node_gen_5/controllers/authController.js
@@ -0,0 +1,44 @@
+import bcrypt from 'bcrypt';
+import User from './models/userModel';
+
+export const register = async (req, res) => {
+    try {
+        // Request body destructuring:
+        const { userName, email, password: psw, role } = req.body;
+
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(psw, salt);
+        const user = await User.create({
+            userName,
+            email,
+            password: hash,
+            role
+        });
+        const { password, ...userData } = user._doc; // userData exclude password
+        return res.status(201).json(userData);
+
+    } catch (error) {
+        console.log('Operation error: ', error);
+        return res.status(500).json({ error: error.message });
+    }
+};
+
+export const login = async (req, res) => {
+    try {
+        const { email, password: PSW } = req.body; // Request body destructuring:
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid password or email.' });
+        }
+        const isValid = await bcrypt.compare(PSW, user.password);
+        if (!isValid) {
+            return res.status(401).json({ message: 'Invalid password or email.' });
+        }
+        const { password, ...userData } = user._doc; // userData exclude password
+        return res.status(200).json(userData);
+
+    } catch (error) {
+        console.log('Operation error: ', error);
+        return res.status(500).json({ error: error.message });
+    }
+};
--- node_gen_5/server.js
+++ node_gen_5/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import './utils/db.js';
+import authRouter from './controllers/authController.js';
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+
+app.use('/api', authRouter);
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
