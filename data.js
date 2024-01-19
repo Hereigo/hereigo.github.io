be503d96b1bdea75cba6e7213b08001994195093
Hereigo <hereigo@online.ua>
2024-01-19 21:14:29 +0200

    cal upd

--- data.js
+++ data.js
@@ -74,4 +74,5 @@ const aData = [
 	{ m: 10, d: 25, c: "a", n: "17:00 AI Beetroot" },
 	{ m: 10, d: 26, c: "a", n: "18:00 Yo Brain Beetroot" },
 	{ m: 10, d: 31, c: "a", n: "16:00 CyberSec Base" },
+	{ m: 02, d: 13, c: "b", n: "PERF.REVIEW" },
 ];
