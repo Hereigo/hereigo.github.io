70fe9eb27347d29156dbabee2685f51cd650bc2b
Hereigo <hereigo@online.ua>
2024-01-25 12:59:35 +0200

    some clean

--- node_gen_3/db.js
+++
@@ -1,9 +0,0 @@
-﻿const mongoose = require('mongoose');
-
-const dbCfg = require('./config/db');
-
-mongoose.connect(dbCfg.URI).then(() => {
-    console.log('Successfully connected to DB.');
-}).catch((err) => {
-    console.err(err);
-});
