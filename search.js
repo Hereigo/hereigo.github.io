1cde5d3a7cf33593e52de8b3638f4e454683af22
Hereigo <hereigo@online.ua>
2024-04-05 10:31:49 +0300

    update srch

--- search.js
+++ search.js
@@ -7,7 +7,8 @@ const mySearchField = document.querySelector('input[type="search"]');
 
 mySearch.addEventListener('click', function () {
   let serachWords = mySearchField.value.toLowerCase().trim().replaceAll(' ', '+');
-  let serachReq = 'https://www.google.com.ua/search?q=' + serachWords + ' -.ru';
+  let serachReq = 'https://www.qwant.com/?hc=0&hti=0&locale=en_GB&q=' + serachWords + '+.ua+-.ru&t=web'
+  // 'https://www.google.com.ua/search?q=' + serachWords + ' -.ru';
   window.open(serachReq);
 });
 
